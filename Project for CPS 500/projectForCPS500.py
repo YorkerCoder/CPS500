0d53f3b6378cc5bda4883d1331b0d08f198c2c84
YorkerCoder <47998506+YorkerCoder@users.noreply.github.com>
2019-11-18 23:33:59 -0500

    Add files via upload

---
+++ Project for CPS 500/projectForCPS500.py
@@ -0,0 +1,128 @@
+#stockClass.py
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt#plots stuff
+import pandas_datareader as web#extracts data off the internet
+from scipy.stats import skew#is a method that lets us get the skew of data
+import random
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+
+class Stock():
+    def __init__(self, ticker, startDate, endDate):
+        self.ticker = ticker
+        self.startDate = startDate
+        self.endDate = endDate
+        self.data = web.get_data_yahoo(ticker, start= startDate, end = endDate)
+
+    #Shows top of the data
+    def display_head(self):
+        print(self.data.head())
+
+    #Plot of the data unmodified
+    def graph_data(self):
+        self.data['Adj Close'].plot()
+        plt.xlabel("Date")
+        plt.ylabel("Adjusted")
+        plt.title(self.ticker + " Prices")
+        plt.show()
+
+    #gets the percent difference between the current and previous day
+    def daily_percent_difference(self):
+        #pct_change() gets the difference between the current and last value
+        #   because there's nothing before the first one it will be NaN and dropna()
+        #   will drop the NaN
+        return self.data['Adj Close'].pct_change().dropna()
+
+    #gets the percent difference between the current and previous month
+    def monthly_percent_difference(self):
+        #ffill() will fill a data frames empty slots with a previous observation
+        #and pct_change() still does the same
+        return self.data['Adj Close'].resample("M").ffill().pct_change()
+
+    #Plot the daily returns
+    def daily_return(self):
+        fig = plt.figure()                                    #From the bottom left
+        axis = fig.add_axes([0.15, 0.1, 0.8 , 0.8])#[x0, y0, width%, height%]
+        axis.plot(self.daily_percent_difference())
+        axis.set_xlabel('Date')
+        axis.set_ylabel('% Difference')
+        axis.set_title(self.ticker + ' Daily Return Data')
+        plt.show()
+
+    #Plot the monthly returns
+    def monthly_return(self):
+        fig = plt.figure()
+        axis = fig.add_axes([0.15 ,0.1 ,0.8, 0.8])#[x0, y0, width%, height%]
+        axis.plot(self.monthly_percent_difference())
+        axis.set_xlabel("Date")
+        axis.set_ylabel("% Difference")
+        axis.set_title(self.ticker + " monthly return data")
+        plt.show()
+
+    #Plot as a histogram of how much of the data % of the data consists of a daily return vs daily returns
+    def histo(self):
+        fig = plt.figure()
+        axis = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+        self.daily_percent_difference().plot.hist(bins = 60)
+        axis.set_xlabel("Daily returns %")
+        axis.set_ylabel("Percent")
+        axis.set_title(self.ticker + " daily returns data")
+        axis.text(-0.35, 200, "Low returns")
+        axis.text(0.25, 200, "High returns")
+        plt.show()
+
+    def histo_gaus(self):
+        #Generate normalized data
+        normalizedData = np.random.normal(self.daily_return_mean(), self.daily_return_std_dev(), size = 10000)
+
+        #Note that alpha is opaqueness going from 0 = transparent to 1 = opaque
+        #   bins is the amount of bins that something will be divided into
+        #   density = true means that the area under the histogram = 1
+
+        plt.hist(normalizedData, bins=150, alpha = 0.8, density = True, label = 'Normal Distribution')
+        plt.hist(self.daily_percent_difference(), bins = 100, alpha = 0.7, density = True, label = 'Returns')
+        plt.legend()
+        plt.xlabel("%")
+        plt.ylabel("% Difference")
+        plt.show()
+        ply/
+    #Get the stats #Note that these are all lambda functions
+    #mean of the daily returns
+    daily_return_mean = lambda self : np.mean(self.daily_percent_difference())
+
+    #standard deviation of the daily returns
+    daily_return_std_dev = lambda self : np.std(self.daily_percent_difference())
+
+    #variance of the daily returns
+    daily_return_variance = lambda self : self.daily_return_std_dev() ** 2
+
+    #mean of the annualized returns
+    annualized_return_mean = lambda self : ((1+ self.daily_return_mean())**252) -1
+
+    #standard deviation of the annualized return
+    annualized_return_std_dev = lambda self : self.daily_return_std_dev()  * np.sqrt(252)
+
+    #variance of the anuualized return
+    annualized_return_variance = lambda self : self.annualized_return_std_dev() ** 2
+
+    #skew of the daily returns
+    daily_skew = lambda self : skew(self.daily_percent_difference())
+
+    #This will print out a neat list of the results
+    def stats(self):
+        print("-"*65)
+        print("The mean of the daily returns is : {0:0.02}%".format(self.daily_return_mean()))
+        print("-"*65)
+        print("The standard deviation of the daily returns is : {0:0.02}%".format(self.daily_return_std_dev()))
+        print("-"*65)
+        print("The variance of the daily returns is : {0:0.02}%".format(self.daily_return_variance()))
+        print("-"*65)
+        print("The skew of the daily returns is : {0:0.02}".format(self.daily_skew()))
+        print("-"*65)
+        print("The mean of the anuualized returns is :  {0:0.02}%".format(self.annualized_return_mean()))
+        print("-"*65)
+        print("The standard deviation of the anuualized returns is  :  {0:0.02}%".format(self.annualized_return_std_dev()))
+        print("-"*65)
+        print("The variance of the anualized returns is :  {0:0.02}%".format(self.annualized_return_variance()))
+        print("-"*65)
---
+++ Project for CPS 500/projectForCPS500GUI.py
@@ -0,0 +1,112 @@
+from projectForCPS500 import *
+from graphics import *
+
+class GUI():
+    def __init__(self,):
+        self.stock = Stock('NFLX', '2009-01-01', '2018-01-1')
+        self.win = GraphWin("Stocks", 400, 400)
+        self.win.setBackground('#ca4b32')
+        self.win.setCoords(0,0,10,10)
+
+
+
+    #For now this will be a static, in the future an implementation will be made
+    #   to make it a dynamic stock handling allowing multiple comparisons of stocks
+    #   and removal of other stocks
+    def wait(self):
+        while True:
+            key = self.win.getKey()
+            if key == 'q':
+                break
+
+    def handle_enter(self):
+        #draw the date text
+        date = Text(Point(4.5,7),"(yyyy-mm-dd)").draw(self.win)
+        #draw the text at the bottom
+        bottom = Text(Point(5,2),"hit enter once you're finished").draw(self.win)
+
+        #the ticker entry and label to the left of it
+        tickerText = Text(Point(1.2,8), "Ticker : ").draw(self.win)
+        tickerEntry = Entry(Point(4, 8),11).draw(self.win)
+
+        #the start date entry and
+        startText =Text (Point(1.5,6), "Start date : ").draw(self.win)
+        startEntry = Entry(Point(4.5,6), 11).draw(self.win)
+
+        endText = Text(Point(1.5,4), "End date   : ").draw(self.win)
+        endEntry = Entry(Point(4.5,4), 11).draw(self.win)
+
+        while True:
+            key = self.win.getKey()
+            if key == "Return":
+                break
+        #undraw everything
+        date.undraw()
+        bottom.undraw()
+        tickerText.undraw()
+        tickerEntry.undraw()
+        startText.undraw()
+        startEntry.undraw()
+        endText.undraw()
+        endEntry.undraw()
+
+        #set the stock
+        self.stock = Stock(tickerEntry.getText(), startEntry.getText(), endEntry.getText())
+
+        #tell them what they can press to get data
+        Text(Point(5,9),"Press q to quit").draw(self.win)
+        Text(Point(5,8),"Press d to get a graph of the data").draw(self.win)
+        Text(Point(5,7),"Press a to get a graph of the daily returns").draw(self.win)
+        Text(Point(5,6),"Press m for a graph of the monthly returns").draw(self.win)
+        Text(Point(5,5),"Press h for a histogram of percent vs daily returns").draw(self.win)
+        Text(Point(5,4),"Press g for a histogram vs a gaussian distribution").draw(self.win)
+        Text(Point(5,3),"Press s for statistics").draw(self.win)
+
+
+
+    def handle_key(self, key):
+        if key  == 'd':
+            #graph the datax
+            self.stock.graph_data()
+
+        elif key == 'a':
+            #graph the daily returns
+            self.stock.daily_return()
+
+        elif key == 'm':
+            #graph the monthly returns
+            self.stock.monthly_return()
+
+        elif key == 'h':
+           #plot a histogram
+            self.stock.histo()
+
+        elif key == 'g':
+            #graph the histogram and gaussian distribution
+            self.stock.histo_gaus()
+
+        elif key == 's':
+            #display the stats
+            self.stock.stats()
+
+        else:
+            print("You did nothing")
+
+
+def main():
+    gui = GUI()
+    #get the stock
+    gui.handle_enter()
+
+    while True:
+        #get their key
+        key = gui.win.getKey()
+        if key == 'q':
+            break
+        gui.handle_key(key)
+
+    gui.win.close()
+
+
+
+main()
